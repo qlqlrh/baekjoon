70a2975cbbab679a37abd34aa080729a1c4cb165
qlqlrh <117878925+qlqlrh@users.noreply.github.com>
2024-01-12 19:08:15 +0900

    [Silver V] Title: 셀프 넘버, Time: 44 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -6,9 +6,8 @@ bool selfNum[10036]; // false인 게 selfNum
 
 int main() {
     for (int i = 1; i <= 10000; i++) { // 모든 숫자에 대해서 d(n) 수행
-        int dn = i;
         int num = i;
-        dn += num % 10;
+        int dn = i + num % 10;
         for (int j = 1; j <= (i / 10); j++) { // 자릿수만큼 반복
             num /= 10;
             dn += num % 10;
