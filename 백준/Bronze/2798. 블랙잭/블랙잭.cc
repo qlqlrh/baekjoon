78e373dc243d470162b9ca853374d80b186827d3
qlqlrh <117878925+qlqlrh@users.noreply.github.com>
2025-01-13 16:46:33 +0900

    Update 블랙잭.cc

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 /**
  * 출력 : M을 넘지 않으면서, 최대한 M에 가까운 카드 3장의 합
- * 구현 : 카드의 최대 개수가 100개라서, 브루투포스 방법으로 풀어도 될 것 같다.
+ * 구현 : 카드의 최대 개수가 100개라서, 브루트포스 방법으로 풀어도 될 것 같다.
  *       가능한 모든 조합을 보면 하나씩 찾아보면 되므로, 0, 1, 2번째 숫자 조합부터 차례대로 조합을 찾는다.
  *       숫자 3개의 합이 M보다 크면 제외하고,
  *       아닌 경우에 대해서만 M과의 차이가 가장 적은 합을 구한다.
@@ -23,7 +24,6 @@ int main() {
     }
 
     int result = 0;
-    int sub = 300000;
     for (int i = 0; i < N; i++) {
         for (int j = i+1; j < N; j++) {
             for (int k = j+1; k < N; k++) {
@@ -31,10 +31,9 @@ int main() {
                 if (sum > M) {
                     continue;
                 }
-                if (M - sum < sub) { // <- STL 사용 가능
-                    sub = M - sum;
-                    result = sum;
-                }
+                // 어차피 sum은 M보다 작으니까
+                // result랑 sum 중에 더 큰 게 M에 가까운 값임!
+                result = max(result, sum);
             }
         }
     }
