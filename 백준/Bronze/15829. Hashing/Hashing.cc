87550cabcc865d4f1e6b98b9cb0baa8d54c2deb0
qlqlrh <117878925+qlqlrh@users.noreply.github.com>
2025-01-14 23:08:06 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+using namespace std;
+
 #define R 31
 #define M 1234567891
-using namespace std;
 
 /**
  * 출력 : 해시 값을 정수로 출력
  * 구현 : 주어진 해시 함수 수식에 맞춰 대입하면 될 것 같다. r = 31, M = 1234567891로 설정.
- * 시간복잡도 : L에 대해서 단일 for문을 돌기 때문에 O(1)이다.
+ * 시간복잡도 : L에 대해서 단일 for문을 돌기 때문에 O(L)이다.
+ * 개선할 점 : pow 함수를 써서 계산을 하려고 했는데,
+ *           이는 부동소수점 연산을 해서 R 값이 클 경우, 반환값이 부정확할 수 있다고 한다.
+ *           몰랐던 부분이라 다음에 pow 함수를 사용할 때 주의해야겠다!
  */
 int main() {
     int L; // 1 ~ 5  or 1 ~ 50
@@ -21,12 +24,12 @@ int main() {
         numList[i] = myChar - 'a' + 1;
     }
 
-    int hash = 0; // 1,234,567,891 보다 작으므로 int형 가능
+    long long hash = 0;
+    long long power = 1;
     for (int i = 0; i < L; i++) {
-        hash += numList[i] * pow(R, i);
+        hash = (hash + numList[i] * power) % M; // pow 함수 쓰면 X
+        power = (power * R) % M;
     }
-
-    hash %= M;
 
     cout << hash << endl;
 
