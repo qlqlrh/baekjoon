53055e8e1c0867f8828f50b05c7320b8d942eb46
qlqlrh <117878925+qlqlrh@users.noreply.github.com>
2025-01-15 18:56:51 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.cc
@@ -1,26 +1,28 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
+/**
+ * 출력: 팰린드롬수 판단 (yes or no)
+ * 구현:
+ * 시간복잡도:
+ */
 int main() {
-    while ( true ) {
+
+    while (true) {
         string num;
         cin >> num;
-        
-        if (num == "0")
-            break;
-        
+
+        if (num == "0") break;
+
         int len = num.length();
-        bool isPelin = true;
-        for (int i = 0; i < len / 2; i++) {
-            if (num[i] != num[len -1 - i]) {
-                cout << "no" << endl;
-                isPelin = false;
+        string isPalin = "yes";
+        for (int i = 0; i < len/2; i++) {
+            if (num[i] != num[len-1-i]) {
+                isPalin = "no";
                 break;
             }
         }
-        if (isPelin)
-            cout << "yes" << endl;
 
+        cout << isPalin << endl;
     }
 }
