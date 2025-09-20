9a3577c0bd036f3a2c6da3d7f751b5dbb3ffd483
qlqlrh <117878925+qlqlrh@users.noreply.github.com>
2025-09-20 22:43:27 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,17 +1,12 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 /**
  * 출력 : M을 넘지 않으면서, 최대한 M에 가까운 카드 3장의 합
- * 구현 : 카드의 최대 개수가 100개라서, 브루트포스 방법으로 풀어도 될 것 같다.
- *       가능한 모든 조합을 보면 하나씩 찾아보면 되므로, 0, 1, 2번째 숫자 조합부터 차례대로 조합을 찾는다.
- *       숫자 3개의 합이 M보다 크면 제외하고,
- *       아닌 경우에 대해서만 M과의 차이가 가장 적은 합을 구한다.
- * 시간복잡도: N까지의 for문이 3개 중첩되어 있으므로, O(N^3)이다.
- *          하지만 N이 최대 100이기 때문에 1,000,000이므로, 1초 안에 연산이 가능하다.
- * 개선할 점 : STL을 더 많이 사용하자!
+ * 구현 : 카드의 최대 개수가 100개라서, 브루투포스 방법으로 풀어도 될 것 같다.
+ * 시간복잡도:
+ *
  */
 int main() {
     int N; // 카드 개수 (3 ~ 100)
@@ -23,17 +18,22 @@ int main() {
         cin >> myVec[i]; // 100,000 이하
     }
 
+    int sum = 0; // 3개 숫자 합
     int result = 0;
+    int sub = 300000;
     for (int i = 0; i < N; i++) {
         for (int j = i+1; j < N; j++) {
             for (int k = j+1; k < N; k++) {
-                int sum = myVec[i] + myVec[j] + myVec[k];
+                sum += myVec[i] + myVec[j] + myVec[k];
                 if (sum > M) {
+                    sum = 0;
                     continue;
                 }
-                // 어차피 sum은 M보다 작으니까
-                // result랑 sum 중에 더 큰 게 M에 가까운 값임!
-                result = max(result, sum);
+                if (M - sum < sub) {
+                    sub = M - sum;
+                    result = sum;
+                }
+                sum = 0;
             }
         }
     }
