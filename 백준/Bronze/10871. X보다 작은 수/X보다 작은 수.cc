e40aa211048b376e9733a734faa188edd97e0248
qlqlrh <117878925+qlqlrh@users.noreply.github.com>
2025-09-20 22:55:08 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -8,14 +8,13 @@ int main() {
 
     int N; // 수열 A의 길이
     int X; // 수열 A에서 판단 기준이 되는 수
-    vector<int> a(N);
-
     cin >> N >> X;
 
+    int num;
     for (int i = 0; i < N; i++) {
-        cin >> a[i];
-        if (X > a[i]) {
-            cout << a[i] << " ";
+        cin >> num;
+        if (X > num) {
+            cout << num << " ";
         }
     }
 }
