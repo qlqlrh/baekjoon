8323262ad6e4094e77780e7cf60d875b52241f9d
qlqlrh <117878925+qlqlrh@users.noreply.github.com>
2025-10-03 20:41:00 +0900

    [Silver V] Title: 영화감독 숌, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,28 +1,25 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-// N번째 종말의 수 출력하기
-// 종말의 수란, 6이 최소 3개 이상 연속으로 들어가는 수
-
 int main() {
-    int N;
-    cin >> N; // 10,000보다 작거나 같은 자연수
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    int cnt = 0;
-    int result = 666;
+	int N; // N번째 영화 (N < 10,000)
+	cin >> N;
 
-    while (true) {
-        if (to_string(result).find("666") != string::npos) { // result 안에 666이 있다면 찾았다면
-            cnt += 1;
-        }
-
-        if (cnt == N)
-            break;
-
-        result += 1;
-    }
-
-    cout << result << endl;
+	int num = 666;
+	int cnt = 0;
+	while (true) {
+		string s = to_string(num);
+		if (s.find("666") != string::npos) {
+			cnt++;
+		}
+		if (cnt == N) {
+			cout << num;
+			break;
+		}
+		num++;
+	}
 }
-
-// Brute Force 알고리즘
