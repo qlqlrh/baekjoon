f78155d90ce8fa2dba104e7414e926549a85ba32
Dongin Kim <117878925+qlqlrh@users.noreply.github.com>
2026-01-15 22:45:49 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define listp(x) for (auto y : x) cout << y << " "
+
+using namespace std;
+
+// 유형 : DP
+// D[i] : i번쨰까지 계산했을 떄의 최솟값
+// D[i] = D[i-1] + (+-v[i])
+// D[0] = v[0]
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    // 입력 처리
+    vector<char> signs;
+    signs.push_back('+');
+    vector<int> numbers;
+    string inp;
+    cin >> inp;
+
+    vector<bool> used(51, true);
+    string number = "";
+    for (char c : inp) {
+        if (isdigit(c)) {
+            number += c;
+        } else {
+            if (c == '-') {
+                used[signs.size()] = false;
+            }
+            signs.push_back(c);
+            numbers.push_back(stoi(number));
+            number = "";
+        }
+    }
+    numbers.push_back(stoi(number));
+
+    // listp(numbers);
+    // cout << "\n";
+    // listp(signs);
+    // cout << "\n";
+    // listp(used);
+    // cout << "\n";
+
+    // 메인 로직
+    int D[51];
+    D[0] = numbers[0];
+    for (int i = 1; i < numbers.size(); i++) {
+        // // 사용 안 한 마이너스 남아있음
+        // int flag = false;
+        // for (int j = 1; j <= i; j++) {
+        //     if (used[j] == false) {
+        //         if (j != i) {
+        //             used[j] = true;
+        //         }
+        //         D[i] = D[i-1] - numbers[i];
+        //         flag = true;
+        //         break;
+        //     }
+        // }
+        //
+        // if (!flag) {
+        //     D[i] = D[i-1] + numbers[i];
+        // }
+        if (signs[i] == '-') D[i] = D[i-1] - numbers[i];
+        else {
+            D[i] = D[i-1] + numbers[i];
+            // +인 경우, 앞에 - 가 있으면 - 가능
+            for (int j = 1; j <= i; j++) {
+                if (signs[j] == '-') {
+                    D[i] = D[i-1] - numbers[i];
+                    break;
+                }
+            }
+        }
+    }
+
+    // listp(D);
+
+    cout << D[numbers.size() - 1] << "\n";
+}
