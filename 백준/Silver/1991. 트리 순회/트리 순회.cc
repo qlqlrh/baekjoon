cd2b5b318a11a3485e67a237becc0fe5cb7d6b45
Dongin Kim <117878925+qlqlrh@users.noreply.github.com>
2026-01-26 18:14:15 +0900

    [Silver I] Title: 트리 순회, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.cc
+++ 백준/Silver/1991. 트리 순회/트리 순회.cc
@@ -1,59 +1,60 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-struct Node {
-    int left = -1;
-    int right = -1;
-};
+// 유형 : 트리
 
-Node tree[26];
+int N; // 26 under
+int p[27];
+int lc[27];
+int rc[27];
 
-void preorder(int cur) {
-    if (cur == -1) return;
-    cout << char(cur + 'A');
-    preorder(tree[cur].left);
-    preorder(tree[cur].right);
+void preorder(int num) {
+    cout << char(num + 65);
+    if (lc[num] != 0) preorder(lc[num]);
+    if (rc[num] != 0) preorder(rc[num]);
+    return;
 }
 
-void inorder(int cur) {
-    if (cur == -1) return;
-    inorder(tree[cur].left);
-    cout << char(cur + 'A');
-    inorder(tree[cur].right);
+void inorder(int num) {
+    if (lc[num] != 0) inorder(lc[num]);
+    cout << char(num + 65);
+    if (rc[num] != 0) inorder(rc[num]);
+    return;
 }
 
-void postorder(int cur) {
-    if (cur == -1) return;
-    postorder(tree[cur].left);
-    postorder(tree[cur].right);
-    cout << char(cur + 'A');
+void postorder(int num) {
+    if (lc[num] != 0) postorder(lc[num]);
+    if (rc[num] != 0) postorder(rc[num]);
+    cout << char(num + 65);
+    return;
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
-    int N;
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
     cin >> N;
+
     for (int i = 0; i < N; i++) {
-        char p, l, r;
-        cin >> p >> l >> r;
-        int parent = p - 'A';
-        if (l == '.') tree[parent].left = -1;
-        else tree[parent].left = l - 'A';
-        if (r == '.') tree[parent].right = -1;
-        else tree[parent].right = r - 'A';
+        char parent, left, right;
+        cin >> parent >> left >> right;
+        if (left != '.')
+           lc[parent - 65] = left - 65;
+        if (right != '.')
+            rc[parent - 65] = right - 65;
+        p[parent - 65] = parent - 65; // A => 0, B => 1, ...
     }
 
-    // 루트는 항상 'A'
-    int root = 0;
+    // cout << 'B'- 65;
 
-    preorder(root);
-    cout << '\n';
-    inorder(root);
-    cout << '\n';
-    postorder(root);
-    cout << '\n';
+    preorder(0);
+    cout << "\n";
 
-    return 0;
+    inorder(0);
+    cout << "\n";
+
+    postorder(0);
+    cout << "\n";
 }
