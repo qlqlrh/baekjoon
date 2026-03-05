40ccc29f53c6ea1650b7cc1533b981f64d392431
Dongin Kim <117878925+qlqlrh@users.noreply.github.com>
2026-03-05 18:03:09 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -0,0 +1,4 @@
+select I.ID as ID, FISH_NAME, LENGTH
+from FISH_INFO I join
+    FISH_NAME_INFO N on I.FISH_TYPE = N.FISH_TYPE
+where (I.FISH_TYPE, LENGTH) in (select FISH_TYPE, MAX(LENGTH) as LENGTH from FISH_INFO group by FISH_TYPE)
