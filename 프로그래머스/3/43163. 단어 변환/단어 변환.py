8e20f2b48a9fff392438ba298609334a3f25da6b
Dongin Kim <117878925+qlqlrh@users.noreply.github.com>
2026-03-31 15:11:47 +0900

    [level 3] Title: 단어 변환, Time: 0.22 ms, Memory: 9.04 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,33 @@
+# BFS (그래프처럼 생각하고 접근해야 함)
+
+from collections import deque
+
+def solution(begin, target, words):
+    answer = 0
+    n = len(words)
+    
+    if target not in words:
+        return 0
+
+    q = deque([(begin, 0)])
+    visit = set([begin])
+
+    def diff(a, b):
+        cnt = 0
+        for i in range(len(a)):
+            if a[i] != b[i]:
+                cnt += 1
+            if cnt >= 2:
+                return False
+        return True
+
+    while q:
+        cw, dist = q.popleft()
+        if cw == target:
+            return dist
+        for word in words:
+            if word not in visit and diff(cw, word):
+                q.append((word, dist + 1))
+                visit.add(word)
+
+    return answer
